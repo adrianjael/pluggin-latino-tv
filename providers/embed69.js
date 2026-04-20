0bf5bea80464cfd3c2dde3a2bffd6dc9cb2d3683
adrianjael <adrianjael15@gmail.com>
2026-04-20 11:31:53 -0500

    chore: bump manifest to v1.2.5.2 final

--- providers/embed69.js
+++ providers/embed69.js
@@ -1,6 +1,6 @@
 /**
  * embed69 - Plugin Nuvio
- * Generado: 2026-04-20T16:30:21.321Z
+ * Generado: 2026-04-20T16:31:53.772Z
  */
 var __defProp = Object.defineProperty;
 var __getOwnPropNames = Object.getOwnPropertyNames;
