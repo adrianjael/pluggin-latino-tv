0e5787242679341600d681ba1a4e91136bd33c38
adrianjael <adrianjael15@gmail.com>
2026-04-27 15:26:19 -0500

    v2.7.0: Resolvers Sync - Integración de resolvedores compartidos en SoloLatino para soporte nativo de VidHide/MinoChinos

--- providers/embed69.js
+++ providers/embed69.js
@@ -1,6 +1,6 @@
 /**
  * embed69 - Plugin Nuvio
- * Generado: 2026-04-27T20:22:17.002Z
+ * Generado: 2026-04-27T20:26:19.499Z
  */
 var __defProp = Object.defineProperty;
 var __getOwnPropNames = Object.getOwnPropertyNames;
